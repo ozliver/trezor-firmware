dc6216f1d5108a826c710fd33444a27eb88996eb
ozliver <cdg728@139.com>
2020-01-14 11:13:54 +0800

    Only compile ok

--- legacy/si2c.c
+++ legacy/si2c.c
@@ -1,12 +1,14 @@
 #include "si2c.h"
+
 #include <errno.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "sys.h"
-#include "usart.h"
 #include "timer.h"
+#include "usart.h"
 
 #if (_SUPPORT_SOFTI2C_ == 0)
 #include <libopencm3/stm32/i2c.h>
@@ -14,39 +16,35 @@
 
 uint8_t g_ucI2cRevBuf[SI2C_BUF_MAX_LEN];
 uint16_t g_usI2cRevLen;
+
+#if !EMULATOR
 static uint8_t s_ucSendDataBak[SI2C_BUF_MAX_LEN];
 static uint16_t s_usSendLenBak;
 static uint8_t s_usTagbak;
 
 #if (_SUPPORT_SOFTI2C_ > 0)
-bool bCheckNFC(void)
-{
-    if (g_ucWorkMode == WORK_MODE_NFC) 
-    {
-        if (GET_NFC_INSERT()) 
-        {
-            return false;
-        }
+bool bCheckNFC(void) {
+  if (g_ucWorkMode == WORK_MODE_NFC) {
+    if (GET_NFC_INSERT()) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 static bool bSI2CDRV_Send_Ack(void) {
   while (1) {
     if (0 == GET_SCL_DAT) break;
-    if(false == bCheckNFC())
-    {
-        return false;
+    if (false == bCheckNFC()) {
+      return false;
     }
-
   }
   SET_I2C_SDA_OUT();
   SET_SDA_LOW;
   while (1) {
     if (GET_SCL_DAT) break;
-    if(false == bCheckNFC())
-    {
-        return false;
+    if (false == bCheckNFC()) {
+      return false;
     }
   }
   return true;
@@ -59,12 +57,10 @@ static uint8_t ucSI2CDRV_Get_Addr(void) {
   /*	i2c_usTimeout = 0;*/
   while (1) {
     if (GET_SCL_DAT) break;
-    
+
     vPower_Control(BUTTON_POWER_OFF);
-    if(PBUTTON_CHECK_READY())
-    {
-      if(true == hasbutton())
-      {
+    if (PBUTTON_CHECK_READY()) {
+      if (true == hasbutton()) {
         return 0xFF;
       }
     }
@@ -73,10 +69,8 @@ static uint8_t ucSI2CDRV_Get_Addr(void) {
   while (1) {
     if (0 == GET_SDA_DAT) break;
     vPower_Control(BUTTON_POWER_OFF);
-    if(PBUTTON_CHECK_READY())
-    {
-      if(true == hasbutton())
-      {
+    if (PBUTTON_CHECK_READY()) {
+      if (true == hasbutton()) {
         return 0xFF;
       }
     }
@@ -84,17 +78,15 @@ static uint8_t ucSI2CDRV_Get_Addr(void) {
   for (bitcount = 0; bitcount < 8; bitcount++) {
     while (1) {
       if (0 == GET_SCL_DAT) break;
-      if(false == bCheckNFC())
-      {
-          return 0xFF;
+      if (false == bCheckNFC()) {
+        return 0xFF;
       }
     }
     SET_I2C_SDA_IN();
     while (1) {
       if (GET_SCL_DAT) break;
-      if(false == bCheckNFC())
-      {
-          return 0xFF;
+      if (false == bCheckNFC()) {
+        return 0xFF;
       }
     }
     iic_slv_addr <<= 1;
@@ -112,28 +104,25 @@ static uint8_t bSI2CDRV_ReadBytes(uint8_t *buf, uint16_t n) {
   uint16_t r0 = 0, r1 = 0;
   // wati addr
   while (SI2C_ADDR != ucSI2CDRV_Get_Addr()) {
-      return false;
+    return false;
   }
-  if(false == bSI2CDRV_Send_Ack())
-  {
-     return false;
+  if (false == bSI2CDRV_Send_Ack()) {
+    return false;
   }
 
   while (!recFinish) {
     for (bitcount = 0; bitcount < 8; bitcount++) {
       while (1) {
         if (0 == GET_SCL_DAT) break;
-        if(false == bCheckNFC())
-        {
-            return false;
+        if (false == bCheckNFC()) {
+          return false;
         }
       }
       SET_I2C_SDA_IN();
       while (1) {
         if (GET_SCL_DAT) break;
-        if(false == bCheckNFC())
-        {
-            return false;
+        if (false == bCheckNFC()) {
+          return false;
         }
       }
       // check STOP,clk:high;dat:low->high
@@ -155,9 +144,8 @@ static uint8_t bSI2CDRV_ReadBytes(uint8_t *buf, uint16_t n) {
       else
         rxbyte |= 0x00;
     }
-    if(false == bSI2CDRV_Send_Ack())
-    {
-       return false;
+    if (false == bSI2CDRV_Send_Ack()) {
+      return false;
     }
     buf[i] = rxbyte;
     i++;
@@ -170,12 +158,11 @@ static bool bSI2CDRV_WriteBytes(uint8_t *buf, uint16_t buf_len) {
 
   uint8_t txbyte, bitcount;
   if ((SI2C_ADDR + 1) != ucSI2CDRV_Get_Addr()) {
-      memset(buf, 0x00, 3);
-      return false;
+    memset(buf, 0x00, 3);
+    return false;
   }
-  if(false == bSI2CDRV_Send_Ack())
-  {
-     return false;
+  if (false == bSI2CDRV_Send_Ack()) {
+    return false;
   }
 
   for (i = 0; i < buf_len; i++) {
@@ -183,9 +170,8 @@ static bool bSI2CDRV_WriteBytes(uint8_t *buf, uint16_t buf_len) {
     for (bitcount = 0; bitcount < 8; bitcount++) {
       while (1) {
         if (0 == GET_SCL_DAT) break;
-        if(false == bCheckNFC())
-        {
-            return false;
+        if (false == bCheckNFC()) {
+          return false;
         }
       }
       SET_I2C_SDA_OUT();
@@ -197,26 +183,23 @@ static bool bSI2CDRV_WriteBytes(uint8_t *buf, uint16_t buf_len) {
       txbyte <<= 1;
       while (1) {
         if (GET_SCL_DAT) break;
-        if(false == bCheckNFC())
-        {
-            return false;
+        if (false == bCheckNFC()) {
+          return false;
         }
       }
     }
 
     while (1) {
       if (0 == GET_SCL_DAT) break;
-      if(false == bCheckNFC())
-      {
-          return false;
+      if (false == bCheckNFC()) {
+        return false;
       }
     }
     SET_I2C_SDA_IN();
     while (1) {
       if (GET_SCL_DAT) break;
-      if(false == bCheckNFC())
-      {
-          return false;
+      if (false == bCheckNFC()) {
+        return false;
       }
     }
     if ((buf_len - 1) == i) {
@@ -224,23 +207,20 @@ static bool bSI2CDRV_WriteBytes(uint8_t *buf, uint16_t buf_len) {
         // wait stop
         while (1) {
           if (0 == GET_SCL_DAT) break;
-          if(false == bCheckNFC())
-          {
-              return false;
+          if (false == bCheckNFC()) {
+            return false;
           }
         }
         while (1) {
           if (GET_SCL_DAT) break;
-          if(false == bCheckNFC())
-          {
-              return false;
+          if (false == bCheckNFC()) {
+            return false;
           }
         }
         while (1) {
           if (GET_SDA_DAT) break;
-          if(false == bCheckNFC())
-          {
-              return false;
+          if (false == bCheckNFC()) {
+            return false;
           }
         }
       }
@@ -410,15 +390,14 @@ uint8_t bSI2CDRV_ReceiveData(uint8_t *pucStr) {
         ucBuf[0] = REPEAT_TAG;
         ucBuf[1] = 0x00;
         ucBuf[2] = 0x00;
-        if(false == bSI2CDRV_WriteBytes(ucBuf, 3))
-        {
-            return false;
+        if (false == bSI2CDRV_WriteBytes(ucBuf, 3)) {
+          return false;
         }
         continue;
       } else {
         s_usTagbak = pucStr[0];
         POWER_OFF_TIMER_CLEAR();
-	      system_millis_poweroff_start = 0;
+        system_millis_poweroff_start = 0;
         return true;
       }
 
@@ -426,13 +405,12 @@ uint8_t bSI2CDRV_ReceiveData(uint8_t *pucStr) {
       if ((REPEAT_TAG == pucStr[0]) && (0x00 == pucStr[1]) &&
           (0x00 == pucStr[2])) {
         SET_COMBUS_LOW();
-        if(false == bSI2CDRV_WriteBytes(s_ucSendDataBak, DATA_HEAD_LEN))
-        {
-            return false;
+        if (false == bSI2CDRV_WriteBytes(s_ucSendDataBak, DATA_HEAD_LEN)) {
+          return false;
         }
-        if(false == bSI2CDRV_WriteBytes(s_ucSendDataBak + DATA_HEAD_LEN, s_usSendLenBak))
-        {
-            return false;
+        if (false == bSI2CDRV_WriteBytes(s_ucSendDataBak + DATA_HEAD_LEN,
+                                         s_usSendLenBak)) {
+          return false;
         }
         continue;
       }
@@ -459,8 +437,7 @@ void vSI2CDRV_SendResponse(uint8_t *pucStr, uint16_t usStrLen) {
   ucHead[1] = ((usStrLen + CRC_LEN) >> 8) & 0xFF;
   ucHead[2] = (usStrLen + CRC_LEN) & 0xFF;
   memcpy(s_ucSendDataBak, ucHead, DATA_HEAD_LEN);
-  if(false == bSI2CDRV_WriteBytes(ucHead, DATA_HEAD_LEN))
-  {
+  if (false == bSI2CDRV_WriteBytes(ucHead, DATA_HEAD_LEN)) {
     POWER_OFF_TIMER_ENBALE();
     system_millis_poweroff_start = 0;
     return;
@@ -476,3 +453,4 @@ void vSI2CDRV_SendResponse(uint8_t *pucStr, uint16_t usStrLen) {
   POWER_OFF_TIMER_ENBALE();
   system_millis_poweroff_start = 0;
 }
+#endif
